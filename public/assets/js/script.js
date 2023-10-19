042ff21f3c7e5b2f5f2f8dd1adf8ac59d7300bd1
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 <picard.boris@gmail.com>
2023-10-19 16:34:39 +0200

    possibilité de relancer les des - ceux garder

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -49,7 +49,7 @@ let diceHtml = document.querySelectorAll(".diceHtml");
 const btnStart = document.querySelector(".btnStart");
 let dices = [];
 let selectDices = [];
-let keepDice = [] ;
+let keepDice = [];
 
 //fonctions
 let dicesRandomNumber = () => { 
@@ -66,37 +66,45 @@ let displayDices = () => {
     const newDices = dicesRandomNumber()
     console.log(newDices.length);
     for(i = 0;i < newDices.length ; i++){
-        diceHtml[i].innerHTML= newDices[i];
+        diceHtml[i].innerHTML = newDices[i];
     }
 };
 
 
 btnStart.addEventListener("click", displayDices);
 
+
+let keepDiceClick = () => {
+    diceHtml.forEach((dice) => {
+    dice.addEventListener("click", (event) => {
+    event.target.classList.add("text-bg-danger");
+    selectDices.push(dice.innerHTML)
+    console.log(selectDices);
+        });
+        return selectDices;
+    });
+};
+keepDiceClick()
+
+
 // L'utilisateur peut garder un nombre de dés entre 1 et 5 :
 
-selectDices = [3,5] ;
+// selectDices = [3,5];
 
 // Stocker le résultat des dés que l'utilisateur va garder : 
-let keepDices = () => {
-    for (let i = 0; i < selectDices.length; i++) {
-        keepDice[i] = selectDices[i]
-    }
-    return keepDice;
-}
-console.log(keepDices());
+// let keepDices = () => {
+//     for (let i = 0; i < selectDices.length; i++) {
+//         keepDice[i] = selectDices[i]
+//     }
+//     return keepDice;
+// }
+// console.log(keepDices());
 
 // Savoir combien de dés il nous reste en fonction du nombre de dés gardés :
 
-let remainingDices = 5 - selectDices.length ;
+// let remainingDices = 5 - selectDices.length ;
 
-console.log(remainingDices);
+// console.log(remainingDices);
 
 
-diceHtml.forEach((element) => {
-    element.addEventListener("click", (event) => {
-    let keepDice = [];
-    console.log(event.target = keepDice);
-        
-})
-})
+
