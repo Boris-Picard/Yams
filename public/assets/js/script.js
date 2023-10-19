668bfa1a06999fea699c32a00e0236e5685e3723
TeddyPruvot <143594565+TeddyPruvot@users.noreply.github.com>
2023-10-19 16:44:07 +0200

    fin jeudi 19/10

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -67,6 +67,71 @@ let displayDices = () => {
     });
 };
 
+//addition des valeurs du tableau sumDice:
+let getSumDices  = () => {
+    let sumDices = dices[0]+dices[1]+dices[2]+dices[3]+dices[4]
+    return sumDices ;
+}
+//affichage du resultat du calcul des valeurs de sumDice
+console.log(getSumDices());
+
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+//afficher chaques valeurs individuellement:
+for (let i = 0; i < dices.length; i++) {
+    const val1 = dices[i];
+    console.log(val1);
+}
+
+//chercher si un élément est présent dans un tableau:
+
+// let elem1 = 1;
+// for (let i=0; i<sumDice.length;i++){
+//     if(elem1 === dices[i]){
+//         console.log('élément contenu');
+//     }
+// }
+
+let value = 1;
+let isFound = getSumDices(value);
+console.log(isFound,"l");
+
+
+
+//Teddy
+
+// switch(dices[i]){
+//     case 1:
+//         if(dices[i]==dices[i])
+//         console.log(dices[i]);
+//         break;
+//     case 2:
+
+//         break;
+//     case 3:
+
+//         break;
+//     case 4:
+
+//     break;
+//     case 5:
+
+//     break;
+//     case 6:
+
+//     break;
+
+// }
+
+
+
+
+
+
 
 btnStart.addEventListener("click", displayDices);
 
