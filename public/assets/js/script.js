6967110a9dad655cc609c0a7f1aa4e8902a3fded
Thibaud Lesot <lesotthibaud@gmail.com>
2023-10-19 11:44:44 +0200

    récupération du js.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -42,3 +42,30 @@
 
 
 //Boris
+
+
+//variables
+let diceHtml = document.querySelectorAll(".diceHtml");
+const btnStart = document.querySelector(".btnStart");
+const dices = [];
+
+//fonctions
+let dicesRandomNumber = () => {
+    for (let index = 0; index < 5; index++) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        dices.push(dice);
+    };
+};
+console.log(dices);
+dicesRandomNumber();
+
+let displayDices = () => {
+    let i = 0;
+    diceHtml.forEach((de) => {
+        de.innerHTML = dices[i];
+        i++;
+    });
+};
+
+
+btnStart.addEventListener("click", displayDices);
