98e61a9ad861807770d1bef6106669028f484f88
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 <picard.boris@gmail.com>
2023-10-19 11:35:08 +0200

    maj

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,8 +4,13 @@
 
 
 
+//variables
 
 
+
+
+
+//fonctions
 
 
 
@@ -18,6 +23,14 @@
 
 
 
+//variables
+
+
+
+
+
+
+//fonctions
 
 
 
@@ -29,3 +42,30 @@
 
 
 //Boris
+
+
+//variables
+let diceHtml = document.querySelectorAll(".diceHtml");
+const btnStart = document.querySelector(".btnStart");
+const dices = [];
+
+//fonctions
+let dicesRandomNumber = () => {
+    for (let index = 0; index < 5; index++) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        dices.push(dice);
+    };
+};
+console.log(dices);
+dicesRandomNumber();
+
+let displayDices = () => {
+    let i = 0;
+    diceHtml.forEach((de) => {
+        de.innerHTML = dices[i];
+        i++;
+    });
+};
+
+
+btnStart.addEventListener("click", displayDices);
