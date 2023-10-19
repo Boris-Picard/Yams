7a7ac61a3257e452cb47a508a9071b25e7672987
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 <picard.boris@gmail.com>
2023-10-19 15:14:52 +0200

    merging

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -45,27 +45,58 @@
 
 
 //variables
-let diceHtml = document.querySelectorAll(".diceHtml");
+let diceHtml = document.querySelectorAll(".diceHtml");        
 const btnStart = document.querySelector(".btnStart");
-const dices = [];
+let dices = [];
+let selectDices = [];
+let keepDice = [] ;
 
 //fonctions
-let dicesRandomNumber = () => {
+let dicesRandomNumber = () => { 
     for (let index = 0; index < 5; index++) {
         let dice = Math.floor(Math.random() * 6) + 1;
         dices.push(dice);
     };
+    return dices
 };
-console.log(dices);
-dicesRandomNumber();
+
 
 let displayDices = () => {
-    let i = 0;
-    diceHtml.forEach((de) => {
-        de.innerHTML = dices[i];
-        i++;
-    });
+    dices = [];
+    const newDices = dicesRandomNumber()
+    console.log(newDices.length);
+    for(i = 0;i < newDices.length ; i++){
+        diceHtml[i].innerHTML= newDices[i];
+    }
 };
 
 
 btnStart.addEventListener("click", displayDices);
+
+// L'utilisateur peut garder un nombre de dés entre 1 et 5 :
+
+selectDices = [3,5] ;
+
+// Stocker le résultat des dés que l'utilisateur va garder : 
+let keepDices = () => {
+    for (let i = 0; i < selectDices.length; i++) {
+        keepDice[i] = selectDices[i]
+    }
+    return keepDice;
+}
+console.log(keepDices());
+
+// Savoir combien de dés il nous reste en fonction du nombre de dés gardés :
+
+let remainingDices = 5 - selectDices.length ;
+
+console.log(remainingDices);
+
+
+diceHtml.forEach((element) => {
+    element.addEventListener("click", (event) => {
+    let keepDice = [];
+    console.log(event.target = keepDice);
+        
+})
+})
