94146b1c5614e16030461bd0f825417a78d7f022
Thibaud Lesot <lesotthibaud@gmail.com>
2023-10-20 16:33:31 +0200

    Brelan en débuggage

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,38 @@
+//Boris
+
+
+//variables
+let diceHtml = document.querySelectorAll(".diceHtml");
+const btnStart = document.querySelector(".btnStart");
+const dices = [];
+
+//fonctions
+let dicesRandomNumber = () => {
+    for (let index = 0; index < 5; index++) {
+        let dice = Math.floor(Math.random() * 6) + 1;
+        dices.push(dice);
+    };
+};
+// console.log(dices);
+dicesRandomNumber();
+
+let displayDices = () => {
+    let i = 0;
+    diceHtml.forEach((de) => {
+        de.innerHTML = dices[i];
+        i++;
+    });
+};
+
+
+btnStart.addEventListener("click", displayDices);
+
+
+
+
+
+
+
 //Teddy 
 
 
@@ -22,7 +57,12 @@
 //variables
 let selectDices = [];
 let keepDice = [] ;
-
+let dicesSameValues = [] ;
+let threeDices = [] ;
+let twoDices = [] ;
+let sameDices = [] ;
+let getKeepDice = [] ;
+let test ;
 
 
 
@@ -34,7 +74,8 @@ let keepDice = [] ;
 
 // L'utilisateur peut garder un nombre de dés entre 1 et 5 :
 
-selectDices = [3,5] ;
+/////////////////////////Valeurs en dur ://///////////////////////////
+ selectDices = [1,1,1,] ;
 
 // Stocker le résultat des dés que l'utilisateur va garder : 
 let keepDices = () => {
@@ -43,44 +84,50 @@ let keepDices = () => {
     }
     return keepDice;
 }
-console.log(keepDices());
+
+getKeepDice = keepDices() ;
+console.log(getKeepDice, "variable retour de la fonction keepDices");
 
 // Savoir combien de dés il nous reste en fonction du nombre de dés gardés :
 
 let remainingDices = 5 - selectDices.length ;
 
-console.log(remainingDices);
+// console.log(remainingDices);
+
+// ============================Brelan==========================================
+// console.log(dices,"valeurs du tableau dices");
+
+// Brelan : Cumul des 3 identiques :
+
+//  Comparer les dés gardés entre eux pour savoir s'ils sont identiques et les stockés dans un tableau " dés identiques" :
+// Pour le tableau des 5 dés, si plusieurs dés ont les mêmes valeurs, on additionne leurs valeurs
 
 
 
+for (let index = 0; index < getKeepDice.length; index++) {
+    let numberOfFive = getKeepDice.filter(number => number === 1);
+    if ( numberOfFive.length ==3){
+        threeDices = getKeepDice[index]*3  
+     }
+
+    
+
+    console.log(numberOfFive);
+
+    }
+
+    // console.log(sameDices,"valeur du tableau sameDices");
+     console.log(threeDices, "log threeDices");
+    // console.log(twoDices, "log twoDices");
 
 
-
-//Boris
-
-
-//variables
-let diceHtml = document.querySelectorAll(".diceHtml");
-const btnStart = document.querySelector(".btnStart");
-const dices = [];
-
-//fonctions
-let dicesRandomNumber = () => {
-    for (let index = 0; index < 5; index++) {
-        let dice = Math.floor(Math.random() * 6) + 1;
-        dices.push(dice);
-    };
-};
-console.log(dices);
-dicesRandomNumber();
-
-let displayDices = () => {
-    let i = 0;
-    diceHtml.forEach((de) => {
-        de.innerHTML = dices[i];
-        i++;
-    });
-};
+//  On stock le cumul des dés qui ont la même valeur  dans une variable brelan.
 
 
-btnStart.addEventListener("click", displayDices);
+// Cumul la valeur des dés :
+let getSumDices  = () => {
+    let sumDices = dices[0]+dices[1]+dices[2]+dices[3]+dices[4]
+    return sumDices ;
+}
+// console.log(getSumDices());
+
