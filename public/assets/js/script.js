e706313cf67dba3e765bd541c92a8c2504c1f162
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 <picard.boris@gmail.com>
2023-10-22 22:58:57 +0200

    maj intégration html css

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,23 +2,40 @@ const diceHtml = document.querySelectorAll(".diceHtml");
 const btnStart = document.querySelector(".btnStart");
 const btnRestart = document.querySelector(".btnRestart");
 const btnValid = document.querySelector(".btnValid");
+const diceImgBoard1 = document.querySelector(".diceImgBoard1");
+const diceImgBoard2 = document.querySelector(".diceImgBoard2");
+const diceImgBoard3 = document.querySelector(".diceImgBoard3");
+const diceImgBoard4 = document.querySelector(".diceImgBoard4");
+const diceImgBoard5 = document.querySelector(".diceImgBoard5");
+const diceImgBoard6 = document.querySelector(".diceImgBoard6");
+let roundCounter = 3;
 let dices = [];
 let selectDices = [];
 
-
+// fonction pour roll 5 dices avec un nombre aléatoire
 let randomDicesNumber = () => {
     for (let index = 0; index < 5; index++) {
         dices.push(Math.floor(Math.random() * 6) + 1);
         console.log(dices);
     };
-    return dices
+    return dices;
 };
 
+
+// fonction qui display les dices dans le board
 let displayRandomDicesNumber = () => {
     randomDicesNumber();
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 5 ; index++) {
         diceHtml[index].innerHTML = dices[index];
-        btnStart.classList.add("d-none")
+        let randomDicesImg = "./public/assets/img/dice"+dices[index]+".png";
+        diceImgBoard1.setAttribute("src", randomDicesImg);
+        diceImgBoard2.setAttribute("src", randomDicesImg);
+        diceImgBoard3.setAttribute("src", randomDicesImg);
+        diceImgBoard4.setAttribute("src", randomDicesImg);
+        diceImgBoard5.setAttribute("src", randomDicesImg);
+        diceImgBoard6.setAttribute("src", randomDicesImg);
+        btnStart.classList.add("d-none");
+        btnRestart.classList.remove("d-none");
     };
 };
 
@@ -37,6 +54,7 @@ let getNumberActiveDice = () => {
             if(dice.classList.contains("active")) {
                 dices.splice(indexInDices, 1);
                 selectDices.push(clickArrayNumber);
+                console.log(selectDices);
                 return selectDices;
             } else {
                 selectDices.splice(indexInSelectDices, 1);
@@ -65,3 +83,19 @@ let restartDices = () => {
 };
 
 btnRestart.addEventListener("click",  restartDices);
+
+
+
+// compteur de manche
+// let roundCounterRemaining = () => {
+//     btnRestart.addEventListener("click", () => {
+//         roundCounter--
+//         console.log(roundCounter);
+//         if(roundCounter === 0) {
+//             btnRestart.classList.add("d-none")
+//         };
+//     });
+// };
+
+// roundCounterRemaining()
+
