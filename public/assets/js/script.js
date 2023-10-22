59cb73d6ab751c3bd854f77172a2b58981977ed1
Thibaud Lesot <lesotthibaud@gmail.com>
2023-10-22 20:27:48 +0200

    Fonction brelan ok

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -75,7 +75,7 @@ let brelan ;
 // L'utilisateur peut garder un nombre de dés entre 1 et 5 :
 
 /////////////////////////Valeurs en dur ://///////////////////////////
-selectDices = [1,1,1,5,5] ;
+selectDices = [2,2,2,3] ;
 
 // Stocker le résultat des dés que l'utilisateur va garder : 
 let keepDices = () => {
@@ -95,38 +95,28 @@ let remainingDices = 5 - selectDices.length ;
 // console.log(remainingDices);
 
 // ============================Brelan==========================================
-// console.log(dices,"valeurs du tableau dices");
 
 // Brelan : Cumul des 3 identiques :
 
-//  Comparer les dés gardés entre eux pour savoir s'ils sont identiques et les stockés dans un tableau " dés identiques" :
-// Pour le tableau des 5 dés, si plusieurs dés ont les mêmes valeurs, on additionne leurs valeurs
 
-
-
+// On parcours le tableau avec une boucle for :
 for (let index = 0; index < getKeepDice.length; index++) {
-    let numberOfFive = getKeepDice.filter(number => number === 1);
-    let sumOfNumber = numberOfFive.length;
-    console.log(sumOfNumber);
-    
-    if (sumOfNumber == 3) {
-    
-    } else {
-        console.log('Pas de 5 dans getKeepDice.');
+    const numberIsOk = getKeepDice[index];
+
+    // console.log(numberIsOk,"log de numberIsOk");
+
+    // On utilise la méthode .filter pour rechercher combien de fois le nombre contenu dans numberIsOk est présent dans le tableau :
+    const howMuchOfNumber = getKeepDice.filter(dice =>dice === numberIsOk).length;
+    // console.log(howMuchOfNumber,"log howMuchOfNumber");
+
+    // Si le nombre présent dans numberIsOk est égale ou SUPERIEUR à trois on le stock dans un constante "Brelan" :
+    if (howMuchOfNumber >= 3) {
+        const brelan = numberIsOk*3;
+        console.log(brelan);
+        console.log("Vous avez un brelan");
+        break;
     }
-    
-
-    
-
-    }
-
-    // console.log(sameDices,"valeur du tableau sameDices");
-    //  console.log(threeDices, "log threeDices");
-    // console.log(twoDices, "log twoDices");
-
-
-//  On stock le cumul des dés qui ont la même valeur  dans une variable brelan.
-
+}
 
 // Cumul la valeur des dés :
 let getSumDices  = () => {
@@ -134,4 +124,3 @@ let getSumDices  = () => {
     return sumDices ;
 }
 // console.log(getSumDices());
-
